91bb77d069fdfc15d64ff98c16d83bc76079fa00
ssmsaito <saito@iflab.tokyo>
2022-03-13 22:14:26 +0900

    english

--- arg_parser.py
+++ arg_parser.py
@@ -1,13 +1,13 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('chord_name', type=str, help='コード名')
-parser.add_argument('--fret-from', type=int, required=False, default=0, help='フレット最小値')
-parser.add_argument('--fret-to', type=int, required=False, default=20, help='フレット最大値')
-parser.add_argument('--fret-range', type=int, required=False, default=3, help='フレット数の幅')
-parser.add_argument('--min-strings', type=int, required=False, default=3, help='演奏する弦の最小本数')
-parser.add_argument('--ignore-root', action='store_true', help='ルート音が違っても出力')
-parser.add_argument('--disable-open-string', action='store_true', help='ポジション外の開放弦を無効化する')
-parser.add_argument('--tuning', type=lambda l: l.split(','), required=False, default=['E','A','D','G','B','E'], help='ギターチューニング')
+parser.add_argument('chord_name', type=str, help='Chord name')
+parser.add_argument('--fret-from', type=int, required=False, default=0, help='Minimum required number for fret position')
+parser.add_argument('--fret-to', type=int, required=False, default=20, help='Maximum required number for fret position')
+parser.add_argument('--fret-range', type=int, required=False, default=3, help='Fret range for diagram suggestion')
+parser.add_argument('--min-strings', type=int, required=False, default=3, help='Minimum required number of played strings')
+parser.add_argument('--ignore-root', action='store_true', help='Allow non-root tone to be the lowest tone if specified')
+parser.add_argument('--disable-open-string', action='store_true', help='Do not allow to have open strings at higher positions if specified')
+parser.add_argument('--tuning', type=lambda l: l.split(','), required=False, default=['E','A','D','G','B','E'], help='Guitar tuning (comma-separated)')
 
 args = parser.parse_args()
